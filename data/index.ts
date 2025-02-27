eee5b5cd72190dca6b748e989aca70b0610f3b81
Flavi Bumba <30435407+YekolaAcademy@users.noreply.github.com>
2025-02-27 22:37:33 +0000

    change the file paths

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const navItems = [
       className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
-      img: "/b1.svg",
+      img: "./b1.svg",
       spareImg: "",
     },
     {
@@ -43,8 +43,8 @@ export const navItems = [
       className: "lg:col-span-2 md:col-span-3 md:row-span-1",
       imgClassName: "",
       titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
+      img: "./grid.svg",
+      spareImg: "./b4.svg",
     },
   
     {
@@ -54,8 +54,8 @@ export const navItems = [
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
+      img: "./b5.svg",
+      spareImg: "./grid.svg",
     },
     {
       id: 6,
@@ -74,7 +74,7 @@ export const navItems = [
       id: 1,
       title: "A project for a client operating in the musical and entertainment industry",
       des: "helps the artist in establishing a strong online presence, showcasing their work, and reaching a wide range of audiences.",
-      img: "/lizikamba.com.png",
+      img: "./lizikamba.com.png",
       iconLists: ["/WordPress-Logo.svg", "/tail.svg", "/ts.svg", "/cloud.svg", "/Siteground-logo.png"],
       link: "/ui.earth.com",
     },
@@ -82,24 +82,24 @@ export const navItems = [
       id: 2,
       title: "Seafood React App",
       des: "Developed a ReactJS Seafood landing page App that leverages an API to fetch seafood data, using Axios, ReactJS, and Bootstrap.",
-      img: "/seafood.png",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/re.svg", "/c.svg"],
+      img: "./seafood.png",
+      iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./re.svg", "./c.svg"],
       link: "/ui.yoom.com",
     },
     {
       id: 3,
       title: "Alcoloco Late Night Liquor Delivery App",
       des: "I am currently working on a late night liquor delivering selling web app using wordpress and woocommerce plugin.",
-      img: "/alcoloco.local.png",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      img: "./alcoloco.local.png",
+      iconLists: ["./re.svg", "./tail.svg", "./ts.svg", "./three.svg", "./c.svg"],
       link: "/ui.aiimg.com",
     },
     {
       id: 4,
       title: "Mordern NextJS Portfolio",
       des: "Developed a modern and responsive portofio using Next.JS, Animation Framer Motion, and TailwindCSS.",
-      img: "/portfolio-nextjs.png",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/re.svg"],
+      img: "./portfolio-nextjs.png",
+      iconLists: ["./next.svg", "./tail.svg", "./ts.svg", "./three.svg", "./re.svg"],
       link: "/ui.aiimg.com",
     },
   ];
@@ -141,32 +141,32 @@ export const navItems = [
     {
       id: 1,
       name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
+      img: "./cloud.svg",
+      nameImg: "./cloudName.svg",
     },
     {
       id: 2,
       name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
+      img: "./app.svg",
+      nameImg: "./appName.svg",
     },
     {
       id: 3,
       name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
+      img: "./host.svg",
+      nameImg: "./hostName.svg",
     },
     {
       id: 4,
       name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
+      img: "./s.svg",
+      nameImg: "./streamName.svg",
     },
     {
       id: 5,
       name: "Siteground.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
+      img: "./dock.svg",
+      nameImg: "./dockerName.svg",
     },
   ];
   
@@ -176,42 +176,42 @@ export const navItems = [
       title: "Web Consulttant - Yekola Digital",
       desc: "Developing WordPress website for clients; from a development stage to deployment- using WordPress Child Theme and Genesis Pro WP Theme Framework.",
       className: "md:col-span-2",
-      thumbnail: "/exp1.svg",
+      thumbnail: "./exp1.svg",
     },
     {
       id: 2,
       title: "Web App Dev Portfolio Project",
       desc: "Desinged a portofio using Nect.JS, Animation Framer Motion, and TailwindCSS. Developed a ReactJS Seafood landing page App that leverages an API to fetch seafood data.",
       className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp2.svg",
+      thumbnail: "./exp2.svg",
     },
     {
       id: 3,
       title: "Freelance WordPress - Developing WordPress website for clients ",
       desc: "A project  for a client operating in a non-profit organisation. A project for a client operating in the musical and entertainment industry .",
       className: "md:col-span-2", // change to md:col-span-2
-      thumbnail: "/exp3.svg",
+      thumbnail: "./exp3.svg",
     },
     {
       id: 4,
       title: "Freelance Back End Project",
       desc: "Developed Online registration app for the IT Conference using PHP and MySQL and applied the CRUD concept. I am currently working on a late night liquor delivering selling web app using wordpress and woocommerce plugin.",
       className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
+      thumbnail: "./exp4.svg",
     },
   ];
   
   export const socialMedia = [
     {
       id: 1,
-      img: "/git.svg",
+      img: "./git.svg",
     },
     {
       id: 2,
-      img: "/twit.svg",
+      img: "./twit.svg",
     },
     {
       id: 3,
-      img: "/link.svg",
+      img: "./link.svg",
     },
   ];
